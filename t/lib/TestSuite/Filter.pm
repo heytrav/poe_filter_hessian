c9318f74744aa7ad08ffadb03124dc9c112c836a
William Travis Holton <wtholton@googlemail.com>
2009-01-29 20:57:03 +0100

    Test base classes

---
+++ t/lib/TestSuite/Filter.pm
@@ -0,0 +1,23 @@
+package  TestSuite::Filter;
+
+use strict;
+use warnings;
+use base 'Test::Class';
+
+
+"one, but we're not the same";
+
+__END__
+
+
+=head1 NAME
+
+TestSuite::Filter - Base class for testing POE::Filter::Hessian
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 INTERFACE
+
+
