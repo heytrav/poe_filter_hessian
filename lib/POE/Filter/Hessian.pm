b3c17024561b913dd8f1ae5748cfa8d61911ecc4
William Travis Holton <wtholton@googlemail.com>
2009-01-29 20:47:22 +0100

    Added the base files for this distribution

---
+++ lib/POE/Filter/Hessian.pm
@@ -0,0 +1,22 @@
+package  POE::Filter::Hessian;
+
+use Moose;
+
+
+"one, but we're not the same";
+
+__END__
+
+
+=head1 NAME
+
+POE::Filter::Hessian - Translate datastructures to and from Hessian for
+transmission Wheel::ReadWrite
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 INTERFACE
+
+
